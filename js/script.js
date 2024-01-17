288a2a78f8ffb4289abc42a71bd2b2b0241897a4
Gormm <juan.cornejo.s@gmail.com>
2024-01-16 23:13:35 -0300

    format

--- js/script.js
+++ js/script.js
@@ -1,67 +1,69 @@
 alert("Bienvenido a TrashPanda Garage");
 const userName1 = "admin";
-const password1 = '1234';
+const password1 = "1234";
 var password2;
 var userName2;
-var loginType = parseInt(prompt( "\n1 Ingresar \n2 Registrar \n3 seguir como invitado "));
+var loginType = parseInt(
+  prompt("\n1 Ingresar \n2 Registrar \n3 seguir como invitado ")
+);
 var newpassword;
 var continuar = true;
 
-// loggin 
-const login = () => {  
-  console.log(userName1 + " " + password1 + " " + userName2 + " " + password2 + " ");
+// loggin
+const login = () => {
+  console.log(
+    userName1 + " " + password1 + " " + userName2 + " " + password2 + " "
+  );
   var newUserName = prompt("Introdusca su nombre de usuario");
-    if (newUserName === userName1 || newUserName === userName2) {
-      newpassword = parseInt(prompt("Introdusca su contraseña"));
-      if  (newpassword == password1 || newpassword == password2){
-        alert("bienvenido un gusto que vuelvas"); 
-        continuar = false;
-        return continuar;
-      }
-      else{
-        alert("contraseña incorrecta");
-      }
-    }else{
-      alert("usuario incorrecto");
+  if (newUserName === userName1 || newUserName === userName2) {
+    newpassword = parseInt(prompt("Introdusca su contraseña"));
+    if (newpassword == password1 || newpassword == password2) {
+      alert("bienvenido un gusto que vuelvas");
+      continuar = false;
+      return continuar;
+    } else {
+      alert("contraseña incorrecta");
     }
-}  
-  
+  } else {
+    alert("usuario incorrecto");
+  }
+};
+
 // registro de nuevo usuario
 const registrar = () => {
-   userName2 = prompt("ingresa tu nombre de usuario" );
-   password2 = prompt("ingresa tu contraseña") ;
+  userName2 = prompt("ingresa tu nombre de usuario");
+  password2 = prompt("ingresa tu contraseña");
   let verificaPass = prompt("reingresa contraseña");
- if (verificaPass == password2) {
-   alert("gracias por registrarte " + userName2);
-   return userName2 , password2;
-   
- }else {
-   alert("contraseña incorrecta");
- }
-}
-
+  if (verificaPass == password2) {
+    alert("gracias por registrarte " + userName2);
+    return userName2, password2;
+  } else {
+    alert("contraseña incorrecta");
+  }
+};
 
 while (loginType != 0 && continuar) {
-    
   switch (loginType) {
-
     case 1:
-            login(); 
+      login();
       break;
-            
-      case 2: 
-           registrar();
+
+    case 2:
+      registrar();
       break;
 
     case 3:
-      alert("bienvenido");   
+      alert("bienvenido");
       continuar = false;
       break;
     default:
       alert("elige una opcion");
-    break;
+      break;
   }
   if (continuar) {
-    loginType = parseInt(prompt("\n1 Ingresar \n2 Registrar \n3 seguir como invitado "), 10);
+    loginType = parseInt(
+      prompt("\n1 Ingresar \n2 Registrar \n3 seguir como invitado "),
+      10
+    );
   }
 }
